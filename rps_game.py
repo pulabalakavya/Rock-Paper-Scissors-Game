a729f3d595a853a85d99a2bfbe868d87fcd335cf
Kavya Pulabala <kavyapulabala@gmail.com>
2025-10-08 17:43:34 +0530

    updated rps_game

--- rps_game.py
+++ rps_game.py
@@ -5,6 +5,7 @@ choices = {
     "scissors":"\U0000270C"
 }
 options=list(choices.keys())
+
 print("Welcome to Rock-Paper-Scissors Game!")
 print("Type 'rock','paper',or 'scissors' . Type 'quit' to exit.")
 
@@ -14,18 +15,19 @@ while True:
     if user_input=="quit":
         print("Thanks for playing!!")
         break
-    if user_input not in options:
+    if user_input not in choices:
         print("Invalid choice, try again.")
         continue
     computer_choice=random.choice(options)
     print(f"you chose:{choices[user_input]}")
-    print(f"Computer chose:{choices[computer_choice]}")
-
+    print(f"computer chose:{choices[computer_choice]}")
+    
     if user_input==computer_choice:
         print("Its a tie!\n")   
     elif((user_input=="rock" and computer_choice=="scissors") or 
     (user_input=="paper" and computer_choice=="rock") or 
     (user_input=="scissors" and computer_choice=="paper")):
-        print("You win! \n")  
+        print("You win!") 
     else:
-        print("computer wins!\n")      
+        print("computer wins!\n")  
+        
